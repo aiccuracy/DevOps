0a49c63c59ce9074924d58df4637d7bb1eda95f6
Soojin An <emily@Soojins-MacBook-Pro.local>
2022-02-20 08:57:23 +0900

    feat: Add user related CRUD

---
+++ api.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const http = require('http');
+const config = require('config');
+const { send } = require('process');
+const { request } = require('https');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+let appPort = config.get('app.port');
+let dbConnect = config.get('db.connect');
+var db;
+
+MongoClient.connect(dbConnect, (error, client) => {
+    if (error) { return console.log(error) };
+    db = client.db('users');
+    app.listen(appPort, () => {
+        console.log("Start");
+    })
+});
+
+
+app.get('/users', async (req, res) => {
+    db.collection('allUsers').find().toArray((error, result) => {
+        res.send(result);
+    });
+});
+
+app.get('/users/:id', async (req, res) => {
+    let id = req.params.id;
+    db.collection('allUsers').findOne({ _id: parseInt(id) }, (error, result) => {
+        if (result) { return res.send(result.name) }
+        else { return res.send(result) };
+    });
+});
+
+
+app.post('/users', async (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    if (name) {
+        db.collection('userCount').findOne({ name: 'counter' }, (error, result) => {
+            var numUser = result.totalUser;
+            db.collection('allUsers').insertOne({ _id: numUser + 1, name: name, age: age }, (error, result) => {
+                console.log(error);
+                db.collection('userCount').updateOne({ name: 'counter' }, { $inc: { totalUser: 1 } }, (error, result) => {
+                    if (error) { return res.send(false) }
+                    else { return res.send(true) };
+                });
+            });
+        })
+    } else {
+        return res.send(false);
+    }
+});
+
+app.put('/users', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    db.collection('allUsers').updateOne({ name: name }, { $set: { age: age } }, (error, result) => {
+        if (error) { return es.send(false) }
+        else if (result.modifiedCount === 1) { return res.send(true) }
+        else { return res.send(false) };
+    })
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.collection('allUsers').deleteOne({ _id: parseInt(id) }, { justOne: true }, (error, result) => {
+        if (error) { return res.send(false) }
+        else if (result.deletedCount === 1) {
+            return res.send(true);
+        } else {
+            return res.send(false);
+        }
+    })
+});
+
+
+
+
