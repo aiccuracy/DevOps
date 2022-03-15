ea8e68ed34de13c2fbd6487571e6c70a7c5cd728
Soojin An <emily@Soojins-MacBook-Pro.local>
2022-03-15 21:13:12 +0900

    feat: Add other CRUD

--- api.js
+++ api.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const { create, readAll, update, deleteOne } = require('./db');
+const {readAll} = require('./db');
 const router = express.Router();
-
 const User = require('./models/user');
-const userCount = require('./models/userCount');
 
 router.get('/users', async (req, res) => {
     const users = await readAll()
@@ -18,5 +16,53 @@ router.get('/users', async (req, res) => {
         users: users.data
     })
 })
+
+router.get('/users/:id', async (req, res) => {
+    User.findOne({ id: req.params.id }, function (err, user) {
+        if (err) return res.status(500).json({ error: err });
+        if (!user) return res.status(404).json({ error: "GET : ID Doesn't exists" });
+        res.json(user);
+    })
+});
+
+router.post('/users/', async (req, res) => {
+    var user = new User();
+    user.name = req.body.name;
+    user.age = req.body.age;
+
+    user.save(function (err) {
+        if (err) {
+            console.error(err);
+            res.json({ message: "Failed to post" });
+            return;
+        }
+        res.json({ message: "Success POST" });
+    });
+});
+
+
+router.put('/users/:id', async (req, res) => {
+    User.findOne({ id: req.params.id }, (err, user) => {
+        if (err) return res.status(500).json({ error: "PUT : Database Failure" });
+        if (!user) return res.status(404).json({ error: "PUT: ID doesn't exist" });
+
+        user.name = req.body.name;
+        user.age = req.body.age;
+        user.save(function (err) {
+            if (err) res.status(500).json({ error: 'Failed to update!' });
+            res.json({ message: 'PUT : SUCCESS' });
+        });
+    });
+});
+
+router.delete('/users/:id', async (req, res) => {
+    User.deleteOne({ id: req.params.id }, function (err, output) {
+        if (err) return res.status(500).json({ error: "DELETE : Database Failure" });
+        res.json({ message: "DELETE : SUCCESS" });
+        res.status(204).end();
+    });
+});
+
+
 module.exports = router;
 
