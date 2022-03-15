c5158f5d376f97f8f15a725f741d57b9c6b1f4f2
Soojin An <emily@Soojins-MacBook-Pro.local>
2022-03-15 21:15:56 +0900

    fix: delete _id column

--- models/user.js
+++ models/user.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const autoIdSetter = require('./auto-id-setter');
+
 
 const userSchema = new Schema({
     name: { type: String, required: true, unique: true },
-    age: {type: Number}
-})
+    age: { type: Number }
+});
+
+autoIdSetter(userSchema, mongoose, 'application', 'id');
+module.exports = userSchema;
 
 module.exports = mongoose.model('userList', userSchema);
