5f74dc067a6ea30f87e5a83eef76d3e87a1a9e7b
Soojin An <emily@Soojins-MacBook-Pro.local>
2022-03-16 00:07:47 +0900

    delete unnecessary schema

--- models/userCount.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userCount = new Schema({
-    name: { type: String },
-    totalUser: { type: Number}
-})
-
-module.exports = mongoose.model('userCount', userCount);
