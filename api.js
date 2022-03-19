4aec06acde32a9150009bade38c51632d0ad4812
Soojin An <emily@Soojins-MacBook-Pro.local>
2022-03-19 21:16:18 +0900

    fix: Error handler - first draft

--- api.js
+++ api.js
@@ -2,56 +2,63 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/user');
 
-
 router.get('/users', async (req, res) => {
     res.send(await User.find({}))
 });
 
-router.get('/users/:id', async (req, res) => {
+router.get('/users/:id', async (req, res, next) => {
     User.findOne({ id: req.params.id }, function (err, user) {
-        if (err) return res.status(500).json({ error: err });
-        if (!user) return res.status(404).json({ error: "GET : ID Doesn't exists" });
+        if (!user) {
+            res.json({ message: "ID Not Found" });
+            return next(err);
+        }
+        if (err) return next(err);
+
         res.json(user);
     })
 });
 
-router.post('/users', async (req, res) => {
+router.post('/users', async (req, res, next) => {
     const user = new User();
     user.name = req.body.name;
     user.age = req.body.age;
 
     user.save(function (err) {
+        if (!user.name) {
+            res.json({ message: "Please write down your name." });
+            return next(err);
+        }
         if (err) {
-            res.json({ message: "Failed to post" });
-            return;
+            return next(err);
         }
         res.json({ message: "Success POST" });
     });
 });
 
 
-router.put('/users/:id', async (req, res) => {
+router.put('/users/:id', async (req, res, next) => {
     User.findOne({ id: req.params.id }, (err, user) => {
-        if (err) return res.status(500).json({ error: "PUT : Database Failure" });
-        if (!user) return res.status(404).json({ error: "PUT: ID doesn't exist" });
-
+        if (!user) {
+            res.json({ message: "ID NOT FOUND" });
+            return next(err);
+        }
+        if (err) return next(err);
         user.name = req.body.name;
         user.age = req.body.age;
-        user.save(function (err) {
-            if (err) res.status(500).json({ error: 'Failed to update!' });
-            res.json({ message: 'PUT : SUCCESS' });
+        user.save(function (error) {
+            if (error) return next(error);
+            res.json({ message: "Successfully updated!" });
         });
     });
 });
 
 router.delete('/users/:id', async (req, res) => {
-    User.deleteOne({ id: req.params.id }, function (err, output) {
-        if (err) return res.status(500).json({ error: "DELETE : Database Failure" });
-        res.json({ message: "DELETE : SUCCESS" });
+    User.deleteOne({ id: req.params.id }, function (err) {
+        if (err) return next(err);
+        res.json({ message: "Successfully deleted" });
         res.status(204).end();
     });
 });
-
 
 module.exports = router;
 
